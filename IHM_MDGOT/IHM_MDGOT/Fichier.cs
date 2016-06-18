f6ec0be7b59857f547ae99f233723aac8b8fced1
Krakinette <wgokraken@gmail.com>
2016-06-18 00:51:14 +0000

    Add files via upload

---
+++ IHM_MDGOT/IHM_MDGOT/Fichier.cs
@@ -0,0 +1,37 @@
+﻿
+
+
+
+using IHM_MDGOT.Modeles;
+using IHM_MDGOT.ViewModels;
+using Métier_MDGOT;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace IHM_MDGOT
+{
+    public class Fichier
+    {
+        public void Save(ObservableCollection<PersonnageViewModel> list)
+        {
+            XmlSerializer xs = new XmlSerializer(typeof(ObservableCollection<PersonnageViewModel>));
+            using (StreamWriter wr = new StreamWriter("Personnage.xml"))
+            {
+                xs.Serialize(wr, list);
+            }
+        
+        }
+
+        public void Load()
+        {
+            XmlSerializer xs = new XmlSerializer(typeof(List<Personnage>));
+            using (StreamReader rd = new StreamReader("Personnage.xml"))
+            {
+                List<Personnage> perso = xs.Deserialize(rd) as List<Personnage>;
+            }
+        }
+    }
+}
+
